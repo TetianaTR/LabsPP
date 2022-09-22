fac87f59c5afb34115fa11a566a30d600495910e
TetianaTR <108334950+TetianaTR@users.noreply.github.com>
2022-09-22 15:19:59 +0200

    Add files via upload

---
+++ Lab3/src/Main.java
@@ -0,0 +1,8 @@
+import Battles.Battle;
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Battle fight=new Battle();
+        fight.startTeamFight();
+    }
+}
