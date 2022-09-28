9bccfd6f731d0593c96908d110dd52e8c2fe0154
TetianaTR <108334950+TetianaTR@users.noreply.github.com>
2022-09-28 21:02:15 +0200

    Delete Lab3 directory

--- Lab3/src/Main.java
+++
@@ -1,8 +0,0 @@
-import Battles.Battle;
-import java.io.IOException;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Battle fight=new Battle();
-        fight.startTeamFight();
-    }
-}
